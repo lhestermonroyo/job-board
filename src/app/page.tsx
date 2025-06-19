a5e4fccca45352edb3bba4632b8b129e420bf802
lhestermonroyo <lhestermoon3011@gmail.com>
2025-06-19 13:33:55 +0800

    setup shadcn and created app layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,36 @@
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarProvider,
+  SidebarTrigger
+} from '@/components/ui/sidebar';
+import { AppSidebarClient } from './_AppSidebarClient';
+
 export default function HomePage() {
   return (
-    <div>
-      <h1>Welcome to the Home Page</h1>
-      <p>This is a simple Next.js application.</p>
-    </div>
+    <SidebarProvider className="overflow-y-hidden">
+      <AppSidebarClient>
+        <Sidebar collapsible="icon" className="overflow-hidden">
+          <SidebarHeader className="flex-row">
+            <SidebarTrigger />
+            <span className="text-xl text-wrap">JobPilot</span>
+          </SidebarHeader>
+          <SidebarContent>Text</SidebarContent>
+          <SidebarFooter>
+            <SidebarMenu>
+              <SidebarMenuItem>
+                <SidebarMenuButton>Button 1</SidebarMenuButton>
+              </SidebarMenuItem>
+            </SidebarMenu>
+          </SidebarFooter>
+        </Sidebar>
+        <main className="flex-1">Content</main>
+      </AppSidebarClient>
+    </SidebarProvider>
   );
 }
